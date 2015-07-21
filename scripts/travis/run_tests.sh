a076f7a634cd4683e68d4d473903ed104a134ac1
Julie Ralph <ju.ralph@gmail.com>
2015-07-21 13:53:08 -0700

    Add travis script

--- scripts/travis/run_tests.sh
+++ scripts/travis/run_tests.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-DART_DIST=dartsdk-linux-x64-release.zip
 DARTIUM_DIST=dartium-linux-x64-release.zip
-
-
-echo -------------------
-echo Fetching dart sdk
-echo -------------------
-
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
-
 
 echo -------------------
 echo Fetching dartium
@@ -20,22 +11,13 @@ echo -------------------
 curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
 
 
-unzip $DART_DIST > /dev/null
 unzip $DARTIUM_DIST > /dev/null
-rm $DART_DIST
 rm $DARTIUM_DIST
 mv dartium-* dartium;
 
 export DART_SDK="$PWD/dart-sdk"
 export PATH="$DART_SDK/bin:$PATH"
 export DARTIUM_BIN="$PWD/dartium/chrome"
-
-
-echo -------------------
-echo Pub install
-echo -------------------
-pub install
-
 
 echo -------------------
 echo Dart analyzer
@@ -50,12 +32,12 @@ fi
 
 
 echo -------------------
-echo Karma-Dart
+echo "Pub run test"
 echo -------------------
 sh -e /etc/init.d/xvfb start
-./node_modules/karma/bin/karma start --single-run --browsers Dartium
+pub run test -p dartium
 
 echo -------------------
-echo Karma-JS
+echo "Pub run test examples"
 echo -------------------
-./node_modules/karma/bin/karma start --single-run --browsers PhantomJS
+pub run test example/example.dart -p dartium
